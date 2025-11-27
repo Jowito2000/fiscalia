9f0613e8e1dd142369e056534a2a113aa01077e6
edsonfe92 <ev.fernandez.2019@alumnos.urjc.es>
2025-11-27 01:26:29 +0100

    no se

--- app/(root)/settings/profile/page.tsx
+++ app/(root)/settings/profile/page.tsx
@@ -1,5 +1,7 @@
-import ProfileLayout from "../layout";
-import ProfileScreen  from "@/components/profile/ProfileScreen";
+import ProfileScreen from "@/components/profile/ProfileScreen";
+
+
+
 
 export default function MyData() {
   return (
