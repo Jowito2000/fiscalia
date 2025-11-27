5471ca2cfe4a6f170f96af93353923a42df5be64
Joel Medina <jowito2000@gmail.com>
2025-11-27 19:21:13 +0100

    fixed background color

--- app/(root)/(other_pages)/layout.tsx
+++ app/(root)/(other_pages)/layout.tsx
@@ -2,7 +2,7 @@ import { HeaderBar } from "@/components/header/HeaderBar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col bg-gray-50">
+    <div className="flex flex-col bg-gray-50 min-h-screen">
       <HeaderBar />
       {children}
     </div>
